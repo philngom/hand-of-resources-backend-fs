439094d9f55b328670534ece593df833d30e7042
Philippe Ngom <philngom@gmail.com>
2022-03-17 20:38:09 -0700

    finished getSingleJoke functionality on route 1/5

--- __tests__/jokes.test.js
+++ __tests__/jokes.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Joke = require('../lib/models/Joke');
 
 describe('hand-of-resources-backend-fr routes', () => {
   beforeEach(() => {
@@ -24,7 +25,29 @@ describe('hand-of-resources-backend-fr routes', () => {
       question: 'Why should you not marry a tennis player',
       punchLine: 'Because Love means nothing to them'
     };
-    console.log(res.body);
     expect(res.body).toEqual(expected);
   });
+
+  test('should get a joke by id', async () => {
+    const joke = await Joke.insert(
+      {
+        type: 'clean',
+        question: 'Why should you not marry a tennis player',
+        punchLine: 'Because Love means nothing to them'
+      }
+    );
+
+    const res = await request(app)
+      .get(`/api/v1/jokes/${joke.id}`);
+
+    const expected = {
+      id: expect.any(String),
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them'
+    };
+
+    expect(res.body).toEqual(expected);
+  });
+
 });

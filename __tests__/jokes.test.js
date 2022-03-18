2924b6892696b69e35324bee3d3d302e5f552121
Philippe Ngom <philngom@gmail.com>
2022-03-17 22:49:45 -0700

    completed 1/5

--- __tests__/jokes.test.js
+++ __tests__/jokes.test.js
@@ -50,4 +50,63 @@ describe('hand-of-resources-backend-fr routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  test('should get all jokes', async () => {
+    await Joke.insert({
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them'
+    });
+
+    const expected = [{
+      id: expect.any(String),
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them'
+    }];
+
+    const res = await request(app)
+      .get('/api/v1/jokes');
+
+    expect(res.body).toEqual(expected);
+  });
+
+  test('should update an existing joke', async () => {
+    const joke = await Joke.insert({
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them'
+    });
+
+    const expected = {
+      id: expect.any(String),
+      type: 'clean',
+      question: 'What two things can you never eat for breakfast?',
+      punchLine: 'Lunch and dinner'
+    };
+
+    const res = await request(app)
+      .patch(`/api/v1/jokes/${joke.id}`)
+      .send({
+        question: 'What two things can you never eat for breakfast?',
+        punchLine: 'Lunch and dinner'
+      });
+
+    expect(res.body).toEqual(expected);
+    expect(await Joke.getSingleJoke(joke.id)).toEqual(expected);
+  });
+
+  test('should delete a joke by id', async () => {
+    const joke = await Joke.insert({
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them'
+    });
+
+    const res = await request(app).delete(`/api/v1/jokes/${joke.id}`);
+
+    expect(res.body).toEqual(joke);
+    // expect(await Joke.getSingleJoke(joke.id)).toBeNull();
+
+  });
+
 });

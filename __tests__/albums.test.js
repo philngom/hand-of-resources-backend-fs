b3eee35a91259dcb5252ee29767899490ce17810
Philippe Ngom <philngom@gmail.com>
2022-03-20 23:53:53 -0700

    completed resource 4/5

--- __tests__/albums.test.js
+++ __tests__/albums.test.js
@@ -32,4 +32,94 @@ describe('Album routes', () => {
     expect(res.body).toEqual(expected);
   });
 
+  test('should get an album by its id', async () => {
+
+    const album = await Album.insert({
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/albums/${album.id}`);
+
+    const expected = {
+      id: expect.any(String),
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    };
+
+    expect(res.body).toEqual(expected);
+
+  });
+
+  test('should get all albums', async () => {
+
+    await Album.insert({
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    });
+
+    const res = await request(app)
+      .get('/api/v1/albums');
+
+    const expected = {
+      id: expect.any(String),
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    };
+
+    expect(res.body).toEqual(expected);
+
+  });
+
+  test('should update an album by id', async () => {
+
+    const album = await Album.insert({
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    });
+
+    const res = await request(app)
+      .patch(`/api/v1/albums/${album.id}`)
+      .send({
+        albumName: 21,
+        producer: 'Adele Adkins'
+      });
+
+    const expected = {
+      id: expect.any(String),
+      artist: 'Adele',
+      albumName: '21',
+      producer: 'Adele Adkins'
+    };
+
+    expect(res.body).toEqual(expected);
+  });
+
+  test('should delete an album by its id', async () => {
+    const album = await Album.insert({
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    });
+
+    const res = await request(app)
+      .delete(`/api/v1/albums/${album.id}`);
+
+    const expected = {
+      id: expect.any(String),
+      artist: 'Adele',
+      albumName: '30',
+      producer: 'Greg Kurstin'
+    };
+
+    expect(res.body).toEqual(expected);
+    expect(await Album.getAlbum(album.id)).toBeNull();
+  });
+
 });

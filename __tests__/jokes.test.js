21093d50acd7d0264a4dd34e614d70365e39ba2d
Philippe Ngom <philngom@gmail.com>
2022-08-02 17:56:32 -0700

    implemented get route

--- __tests__/jokes.test.js
+++ __tests__/jokes.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Joke = require('../lib/models/Joke');
 
 describe('hand-of-resources-backend-fr routes', () => {
   beforeEach(() => {
@@ -12,19 +13,38 @@ describe('hand-of-resources-backend-fr routes', () => {
     pool.end();
   });
 
-  test('should add a joke to the db', async () => {
-
-    const res = await request(app)
-      .post('/api/v1/jokes')
-      .send({ type: 'clean', question: 'Why should you not marry a tennis player', punchLine: 'Because Love means nothing to them' });
+  test.skip('should add a joke to the db', async () => {
+    const res = await request(app).post('/api/v1/jokes').send({
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them',
+    });
 
     const expected = {
       id: expect.any(String),
       type: 'clean',
       question: 'Why should you not marry a tennis player',
-      punchLine: 'Because Love means nothing to them'
+      punchLine: 'Because Love means nothing to them',
     };
-    console.log(res.body);
     expect(res.body).toEqual(expected);
+  });
+
+  test('should get a joke from the db', async () => {
+    await Joke.insert({
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them',
+    });
+
+    const expected = {
+      id: expect.any(String),
+      type: 'clean',
+      question: 'Why should you not marry a tennis player',
+      punchLine: 'Because Love means nothing to them',
+    };
+
+    const res = await request(app).get('/api/v1/jokes');
+
+    expect(res.body).toEqual([expected]);
   });
 });
